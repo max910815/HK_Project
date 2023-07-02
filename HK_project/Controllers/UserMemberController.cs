2e2ee249f53a5ae955d6a1a8f34dcf4c9e528627
max910815 <a0966474739@gmail.com>
2023-07-02 21:18:55 +0800

    first

---
+++ HK_project/Controllers/UserMemberController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HK_project.Controllers
+{
+    public class UserMemberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
